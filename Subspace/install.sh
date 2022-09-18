0e129f6a8f78a6081770dface0ce865c1609b4ae
ericet <ericetchen@gmail.com>
2022-09-19 01:51:07 +0200

    add subspace

---
+++ Subspace/install.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+while true
+do
+
+# Logo
+
+echo "============================================================"
+curl -s https://raw.githubusercontent.com/ericet/easynodes/master/logo.sh | bash
+echo "============================================================"
+
+
+source ~/.profile
+
+PS3='选择一个操作 '
+options=(
+"安装必要的环境" 
+"安装节点" 
+"查看节点同步状态" 
+"升级节点"
+"退出")
+select opt in "${options[@]}"
+               do
+                   case $opt in
+                   
+"安装必要的环境")
+echo "============================================================"
+echo "准备开始。。。"
+echo "============================================================"
+
+#INSTALL DEPEND
+echo "============================================================"
+echo "Update and install APT"
+echo "============================================================"
+sleep 3
+sudo apt update && sudo apt upgrade -y && \
+sudo apt install curl jq ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
+
+
+echo "============================================================"
+echo "服务器环境准备好了!"
+echo "============================================================"
+break
+;;
+            
+"安装节点")
+echo "============================================================"
+echo "输入节点的名称:"
+echo "============================================================"
+                
+read NODENAME
+NODENAME=$NODENAME
+echo 'export NODENAME='${NODENAME} >> $HOME/.profile
+
+echo "============================================================"
+echo "输入钱包地址:"
+echo "============================================================"
+               
+read WALLET_ADDRESS
+WALLET_ADDRESS=$WALLET_ADDRESS
+echo 'export WALLET_ADDRESS='${WALLET_ADDRESS} >> $HOME/.profile
+PLOT_SIZE=100G
+echo 'export PLOT_SIZE='${PLOT_SIZE} >> $HOME/.profile
+source $HOME/.profile
+
+echo "============================================================"
+echo "节点安装开始。。。"
+echo "============================================================"
+
+cd $HOME
+rm -rf subspace-*
+APP_VERSION=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name" | sed "s/runtime-/""/g")
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-node-ubuntu-x86_64-${APP_VERSION}
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-farmer-ubuntu-x86_64-${APP_VERSION}
+chmod +x subspace-*
+mv subspace-* /usr/local/bin/
+
+
+tee $HOME/subspaced.service > /dev/null <<EOF
+[Unit]
+Description=Subspace Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which subspace-node) --chain gemini-2a --execution wasm --state-pruning archive --validator --name $NODENAME
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+mv $HOME/subspaced.service /etc/systemd/system/
+
+tee $HOME/subspaced-farmer.service > /dev/null <<EOF
+[Unit]
+Description=Subspaced Farm
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which subspace-farmer) farm --reward-address $WALLET_ADDRESS --plot-size $PLOT_SIZE
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+mv $HOME/subspaced-farmer.service /etc/systemd/system/
+
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced subspaced-farmer
+sudo systemctl restart subspaced
+sleep 30
+sudo systemctl restart subspaced-farmer
+
+
+echo "============================================================"
+echo "节点安装成功!"
+echo "============================================================"
+break
+;;
+
+
+
+"查看节点同步状态")
+curl -s -X POST http://localhost:9933 -H "Content-Type: application/json" --data '{"id":1, "jsonrpc":"2.0", "method": "system_health", "params":[]}' | jq .result.isSyncing
+break
+;;
+
+"升级节点")
+cd $HOME && rm -rf subspace-*
+APP_VERSION=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name" | sed "s/runtime-/""/g")
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-node-ubuntu-x86_64-${APP_VERSION}
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/${APP_VERSION}/subspace-farmer-ubuntu-x86_64-${APP_VERSION}
+chmod +x subspace-*
+mv subspace-* /usr/local/bin/
+systemctl restart subspaced
+sleep 30
+systemctl restart subspaced-farmer
+
+break
+;;
+
+"退出")
+exit
+;;
+
+*) echo "invalid option $REPLY";;
+esac
+done
+done
